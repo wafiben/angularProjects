a935eff1368fc73d619a52f4ee6da316d021e306
unknown <wafibenjeddou@gmail.com>
2021-03-20 22:24:08 +0100

    import firebase and add tooting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import "firebase/analytics";
+import firebase from "firebase/app";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+  constructor(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyDknPe_SobNMbTo8lFg4FGoJy7Qg9_QgrM",
+    authDomain: "bookshelves-7a770.firebaseapp.com",
+    projectId: "bookshelves-7a770",
+    storageBucket: "bookshelves-7a770.appspot.com",
+    messagingSenderId: "896835585826",
+    appId: "1:896835585826:web:7007ee1fcfe646cf4e477b",
+    measurementId: "G-ZHXPDR0DRT"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
 }
